ac869b18770a0208bf7c5d7d95baa976b32dfaac
cc23121 <143607741+cc23121@users.noreply.github.com>
2023-11-14 11:27:42 -0300

    url_java
    oh fé cabelin na voz

--- TecPro-II/JAVA/servidor/src/Filme.java
+++ TecPro-II/JAVA/servidor/src/Filme.java
@@ -2,38 +2,41 @@ import java.time.LocalDate;
 
 public class Filme {
 
-        private int idFilme;
-        private String titulo;
-        private String diretor;
-        private int anoLancamento;
-        private String sinopse;
-        private float nota;
+    private int idFilme;
+    private String titulo;
+    private String diretor;
+    private int anoLancamento;
+    private String sinopse;
+    private float nota;
+    private String url_foto;
 
-        public Filme(String titulo, String diretor, int anoLancamento, String sinopse, float nota) {
-            this.titulo = titulo;
-            this.diretor = diretor;
-            if (anoLancamento <= LocalDate.now().getYear()){
-                this.anoLancamento = anoLancamento;
-            }
-            this.sinopse = sinopse;
-            if (nota <= 10 || nota >= 0){
-                this.nota = nota;
-            }
-
+    public Filme(String titulo, String diretor, int anoLancamento, String sinopse, float nota,  String url_foto) {
+        this.titulo = titulo;
+        this.diretor = diretor;
+        if (anoLancamento <= LocalDate.now().getYear()){
+            this.anoLancamento = anoLancamento;
         }
-        public Filme(int idFilme, String titulo, String diretor, int anoLancamento, String sinopse, float nota) {
-            this.idFilme = idFilme;
-            this.titulo = titulo;
-            this.diretor = diretor;
-            if (anoLancamento <= (LocalDate.now().getYear())){
-                this.anoLancamento = anoLancamento;
-            }
-            this.sinopse = sinopse;
-            if (nota <= 10 || nota >= 0){
-                this.nota = nota;
-            }
-
+        this.sinopse = sinopse;
+        if (nota <= 10 || nota >= 0){
+            this.nota = nota;
         }
+        this.url_foto = url_foto;
+
+    }
+    public Filme(int idFilme, String titulo, String diretor, int anoLancamento, String sinopse, float nota, String url_foto) {
+        this.idFilme = idFilme;
+        this.titulo = titulo;
+        this.diretor = diretor;
+        if (anoLancamento <= (LocalDate.now().getYear())){
+            this.anoLancamento = anoLancamento;
+        }
+        this.sinopse = sinopse;
+        if (nota <= 10 || nota >= 0){
+            this.nota = nota;
+        }
+        this.url_foto = url_foto;
+
+    }
 
 
     public int getIdFilme() {
@@ -45,50 +48,58 @@ public class Filme {
     }
 
     public String getTitulo() {
-            return titulo;
-        }
-
-        public void setTitulo(String titulo) {
-            this.titulo = titulo;
-        }
-
-        public String getDiretor() {
-            return diretor;
-        }
-
-        public void setDiretor(String diretor) {
-            this.diretor = diretor;
-        }
-
-        public int getAnoLancamento() {
-            return anoLancamento;
-        }
-
-        public void setAnoLancamento(int anoLancamento) {
-            this.anoLancamento = anoLancamento;
-        }
-
-        public String getSinopse() {
-            return sinopse;
-        }
-
-        public void setSinopse(String sinopse) {
-            this.sinopse = sinopse;
-        }
-
-
-        public float getNota() {
-            return nota;
-        }
-
-        public void setNota(float nota) {
-            this.nota = nota;
-        }
-
-        @Override
-        public String toString() {
-            return titulo;
-        }
-
-
+        return titulo;
     }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getDiretor() {
+        return diretor;
+    }
+
+    public void setDiretor(String diretor) {
+        this.diretor = diretor;
+    }
+
+    public int getAnoLancamento() {
+        return anoLancamento;
+    }
+
+    public void setAnoLancamento(int anoLancamento) {
+        this.anoLancamento = anoLancamento;
+    }
+
+    public String getSinopse() {
+        return sinopse;
+    }
+
+    public void setSinopse(String sinopse) {
+        this.sinopse = sinopse;
+    }
+
+
+    public float getNota() {
+        return nota;
+    }
+
+    public String getUrl_foto() {
+        return url_foto;
+    }
+
+    public void setUrl_foto(String url_foto) {
+        this.url_foto = url_foto;
+    }
+
+    public void setNota(float nota) {
+        this.nota = nota;
+    }
+
+    @Override
+    public String toString() {
+        return titulo;
+    }
+
+
+}
